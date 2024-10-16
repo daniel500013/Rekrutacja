9ae39f9916123c2ac0d807f80331ff7589a28270
Daniel Szeląg <bakucraft229@gmail.com>
2024-10-17 00:02:18 +0200

    [Parser helper] Added validation for ParserHelper

--- Rekrutacja/Rekrutacja/Workers/Helper/ParserHelper.cs
+++ Rekrutacja/Rekrutacja/Workers/Helper/ParserHelper.cs
@@ -1,9 +1,31 @@
-﻿namespace Rekrutacja.Workers.Helper
+﻿using System;
+using System.Linq;
+
+namespace Rekrutacja.Workers.Helper
 {
     public static class ParserHelper
     {
         public static int StringToInt(string str)
         {
+            #region Validation
+            if (string.IsNullOrEmpty(str))
+                throw new ArgumentException("Podany ciąg znaków jest pusty");
+
+            if (str == "-")
+                throw new FormatException("Podano sam znak minus bez liczby");
+
+            char[] numberChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i == 0 && str[i] == '-')
+                    continue;
+                
+                if (!numberChars.Contains(str[i]))
+                    throw new FormatException($"Niedozwolony znak '{str[i]}'. Dozwolone są tylko cyfry i znak minus na początku.");
+            }
+            #endregion
+
             bool isNegative = false;
 
             if (str.StartsWith("-"))
