a4efbe3f67ad4886bef0707a5136fdd43b97aaed
Daniel Szeląg <bakucraft229@gmail.com>
2024-10-16 23:50:03 +0200

    [Parser worker] Fix single '-' char bug

--- Rekrutacja/Rekrutacja/Extensions/StringExtensions.cs
+++ Rekrutacja/Rekrutacja/Extensions/StringExtensions.cs
@@ -9,9 +9,10 @@ namespace Rekrutacja.Extensions
             char[] numberChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
             if (string.IsNullOrEmpty(str))
-            {
                 return true;
-            }
+
+            if (str == "-")
+                return true;
 
             for (int i = 0; i < str.Length; i++)
             {
